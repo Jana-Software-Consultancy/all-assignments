f5bc97dfe16f62000eb494208a1dc47c1a63baea
Sagnik <sagnikjanacodes@gmail.com>
2024-05-13 06:36:49 +0530

    Partially solved Expenditure Analysis

--- week-1/Week-1-assignment-with-tests/01-js/easy/expenditure-analysis.js
+++ week-1/Week-1-assignment-with-tests/01-js/easy/expenditure-analysis.js
@@ -1,12 +1,48 @@
-/*
-  Implement a function `calculateTotalSpentByCategory` which takes a list of transactions as parameter
-  and return a list of objects where each object is unique category-wise and has total price spent as its value.
-  Transaction - an object like { itemName, category, price, timestamp }.
-  Output - [{ category1 - total_amount_spent_on_category1 }, { category2 - total_amount_spent_on_category2 }]
-
-  Once you've implemented the logic, test your code by running
-  - `npm run test-expenditure-analysis`
-*/
+function solve(transactions) {
+  var spendEstimates = {};
+  for (var i = 0; i < transactions.length; i++) {
+    var t = transactions[i];
+    if (spendEstimates[t.category]) {
+      spendEstimates[t.category] = spendEstimates[t.category] + t.price;
+    } else {
+      spendEstimates[t.category] = t.price;
+    }
+  }
+  console.log(spendEstimates);
+}
+var transactions = [
+  {
+    itemName: "kachori",
+    category: "food",
+    price: 100,
+    timestamp: "04-April-2024",
+  },
+  {
+    itemName: "Biscuits",
+    category: "food",
+    price: 100,
+    timestamp: "04-April-2024",
+  },
+  {
+    itemName: "Flight Bills",
+    category: "travel",
+    price: 100,
+    timestamp: "04-April-2024",
+  },
+  {
+    itemName: "Petrol",
+    category: "travel",
+    price: 100,
+    timestamp: "04-April-2024",
+  },
+  {
+    itemName: "Samosa",
+    category: "food",
+    price: 100,
+    timestamp: "04-April-2024",
+  },
+  
+];
 
 function calculateTotalSpentByCategory(transactions) {
   return [];
