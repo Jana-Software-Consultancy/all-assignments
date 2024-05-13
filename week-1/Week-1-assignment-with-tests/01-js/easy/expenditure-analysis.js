65b1a5ac06811970d79d6245957c22cf2dfc1d9a
Sagnik <sagnikjanacodes@gmail.com>
2024-05-13 13:30:27 +0530

    updating expenditure analysis as per test cases

--- week-1/Week-1-assignment-with-tests/01-js/easy/expenditure-analysis.js
+++ week-1/Week-1-assignment-with-tests/01-js/easy/expenditure-analysis.js
@@ -1,51 +1,28 @@
-function solve(transactions) {
+function calculateTotalSpentByCategory(transactions) {
   var spendEstimates = {};
+
   for (var i = 0; i < transactions.length; i++) {
     var t = transactions[i];
     if (spendEstimates[t.category]) {
-      spendEstimates[t.category] = spendEstimates[t.category] + t.price;
+      spendEstimates[t.category] += t.price;
     } else {
       spendEstimates[t.category] = t.price;
     }
   }
-  console.log(spendEstimates);
-}
-var transactions = [
-  {
-    itemName: "kachori",
-    category: "food",
-    price: 100,
-    timestamp: "04-April-2024",
-  },
-  {
-    itemName: "Biscuits",
-    category: "food",
-    price: 100,
-    timestamp: "04-April-2024",
-  },
-  {
-    itemName: "Flight Bills",
-    category: "travel",
-    price: 100,
-    timestamp: "04-April-2024",
-  },
-  {
-    itemName: "Petrol",
-    category: "travel",
-    price: 100,
-    timestamp: "04-April-2024",
-  },
-  {
-    itemName: "Samosa",
-    category: "food",
-    price: 100,
-    timestamp: "04-April-2024",
-  },
-  
-];
 
-function calculateTotalSpentByCategory(transactions) {
-  return [];
+  var keys = Object.keys(spendEstimates);
+  var answer = [];
+
+  for (var i = 0; i < keys.length; i++) {
+    var category = keys[i];
+    var obj = {
+      category: category,
+      totalSpent: spendEstimates[category]
+    }
+    answer.push(obj);
+  }
+
+  return answer;
 }
 
 module.exports = calculateTotalSpentByCategory;
